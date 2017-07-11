76d4c5a804d1b9243de921e3f529177b1bd3bea4
Alexandre Page <alexandre.page@epitech.eu>
2017-07-11 19:31:55 +0200

    Custom Call Activity and Task working

---
+++ src/extendingruntime/ImageManager.java
@@ -0,0 +1,84 @@
+package extendingruntime;
+
+import java.net.URL;
+
+import org.eclipse.bpmn2.modeler.core.runtime.CustomTaskDescriptor;
+import org.eclipse.bpmn2.modeler.core.runtime.CustomTaskImageProvider.IconSize;
+import org.eclipse.bpmn2.modeler.core.runtime.TargetRuntime;
+import org.eclipse.graphiti.ui.internal.GraphitiUIPlugin;
+import org.eclipse.graphiti.ui.platform.AbstractImageProvider;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.ImageRegistry;
+
+import bpmn2.tutorial.ImixsRuntimeExtension;
+
+public class ImageManager extends AbstractImageProvider {
+
+	public final static String IMAGE_ID_PREFIX = ImageManager.class.getPackage().getName() + ".";
+	public final static String ICONS_FOLDER = "icons/";
+	
+	public enum IconSize {
+		SMALL("small"),
+		LARGE("large"),
+		HUGE("huge");
+		private String value;
+		private IconSize(String value) {
+			this.value = value;
+		}
+	}
+	
+	private static boolean registered = false;
+	
+	public ImageManager() {
+		super();
+	}
+	
+	public static void registerAvailableImages() {
+		if (!registered) {
+			ImageRegistry imageRegistry = GraphitiUIPlugin.getDefault().getImageRegistry();
+			TargetRuntime rt = TargetRuntime.getRuntime(ImixsRuntimeExtension.RUNTIME_ID);
+			for (IconSize size : IconSize.values()) {
+				for (CustomTaskDescriptor ctd : rt.getCustomTaskDescriptors()) {
+					String imageId = getImageId(ctd,size); 
+					if (imageId != null) {
+						String filename = getImagePath(ctd,size);
+						URL url = ImageManager.class.getClassLoader().getResource(filename);
+						ImageDescriptor descriptor =  ImageDescriptor.createFromURL(url);
+						imageRegistry.put(imageId, descriptor);
+					}
+				}
+			}
+			registered = true;
+		}
+	}
+	
+	@Override
+	protected void addAvailableImages() {
+		TargetRuntime rt = TargetRuntime.getRuntime(ImixsRuntimeExtension.RUNTIME_ID);
+		for (IconSize size : IconSize.values()) {
+			for (CustomTaskDescriptor ctd : rt.getCustomTaskDescriptors()) {
+				String imageId = getImageId(ctd,size); 
+				if (imageId != null) {
+					addImageFilePath(imageId, getImagePath(ctd,size));
+				}
+			}
+		}
+	}
+	
+	public static String getImageId(CustomTaskDescriptor ctd, IconSize small) {
+		String icon = (String) ctd.getPropertyValue("icon"); 
+		if (icon != null && icon.trim().length() > 0) {
+			return IMAGE_ID_PREFIX + icon.trim() + "." + small.value;
+		}
+		return null;
+	}
+	
+	public static String getImagePath(CustomTaskDescriptor ctd, IconSize size) {
+		String icon = (String) ctd.getPropertyValue("icon"); 
+		if (icon != null && icon.trim().length() > 0) {
+			return ICONS_FOLDER + size.value + "/" + icon.trim();
+		}
+		return null;
+	}
+	
+}
